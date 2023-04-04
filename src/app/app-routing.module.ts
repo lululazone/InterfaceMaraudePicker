ac6356b3e69eff74efce06ca666c5487c8358d41
lululazone <girardlulu@gmail.com>
2023-04-04 17:29:39 +0200

    [ADD] Pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {FicheContactComponent} from "./fiche-contact/fiche-contact.component";
+import {AppComponent} from "./app.component";
+import {MainComponentComponent} from "./main-component/main-component.component";
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'about', component:FicheContactComponent},{ path: '', component:MainComponentComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FicheContactComponent } from './fiche-contact/fiche-contact.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponentComponent } from './main-component/main-component.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FicheContactComponent,
+    HeaderComponent,
+    MainComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    AppRoutingModule,
+      BrowserAnimationsModule,
+      TuiRootModule,
+      TuiDialogModule,
+      TuiAlertModule
+],
+  providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
